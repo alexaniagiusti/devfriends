329ce484d00697f7f666fc1962d3634da82489fb
Alexânia Giusti <alexaniagiusti@hotmail.com>
2019-05-02 23:13:50 -0300

    inserção de conteúdo na página sobre

--- src/views/Sobre.vue
+++ src/views/Sobre.vue
@@ -1,7 +1,27 @@
 <template>
-  <div class="sobre">
-    <img class="imagem1" src="https://firebasestorage.googleapis.com/v0/b/devfriends-5ab33.appspot.com/o/Fundo%20do%20Site%2Fundraw_static_assets_rpm6.svg?alt=media&token=01aecb34-da03-426e-b124-ba4256b26956" height="400px">
+  <div>
+    <div class="sobre">
+      <img class="box" src="https://firebasestorage.googleapis.com/v0/b/devfriends-5ab33.appspot.com/o/Fundo%20do%20Site%2Fhtml5vuecssjs.png?alt=media&token=e6c24f5b-e503-49b2-a769-d44b0299927a" height="400px">
+    <div class="box2">
+      <span class="headline white--text"> Precisando dar um up na sua empresa?</span>
+      <span class="subheading white--text text-xs-center mt-3 font-weight-thin"> 
+        A DevFriends adoraria ajudar, oferecemos a você
+        empreendedor, sistemas que automatizarão os processos 
+        da sua empresa, trabalhamos para entregar produtos de alta 
+        performance e conteúdos que agreguem valor aos nossos clientes 
+        e visitantes, nosso trabalho é baseado em feedback contínuo 
+        através da utilização de metodologias ágeis de desenvolvimento, 
+        não trabalhamos com escopo fechado, quem decide quando o produto
+        está pronto são nossos clientes, pois deixá-los contentes e satisfeitos
+        é a nossa prioridade, por isso garantimos um desenvolvimento altamente 
+        interativo, entre em contato conosco e solicite um orçamento, 
+        nós desenvolvemos aplicações ao estilo single page applications 
+        e progressive web apps.
+      </span>
+    </div>
+    </div>
   </div>
+
 </template>
 
 <script>
@@ -17,11 +37,24 @@ export default {
   width: 100%;
   height: 100vh;
   background-color: #6A1B9A;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
 }
 
-.imagem1 {
-  margin-top: 100px;
-  margin-left: 130px;
+.box {
+  display: flex;
+  justify-content: center;
+  align-items: center
 }
+.box2 {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 450px;
+  margin-left: 30px;
+  margin-top: 20px;
+  }
 
 </style>
